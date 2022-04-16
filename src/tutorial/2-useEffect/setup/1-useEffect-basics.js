6e846c41e20fa4e30c339842e6d52d0d3bef5c1d
Yirong Qin <yq4103@mun.ca>
2022-04-16 00:38:23 -0400

    use effect

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -3,7 +3,17 @@ import React, { useState, useEffect } from 'react';
 // cleanup function
 // second parameter
 const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
+  const [value, setValue] = useState(0);
+  useEffect(()=>{
+    if (value > 0) {
+      document.title = `New Messages(${value})`;
+    }
+  },[value]);
+
+  return <>
+    <h1>{value}</h1>
+    <button className='btn' onClick={()=> setValue(value + 1)}>Click</button>
+  </>;
 };
 
 export default UseEffectBasics;
